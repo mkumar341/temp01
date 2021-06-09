ab9266d0c505160697d302706da678bc76242fb0
Mukesh Kumar <mukkum@studylearnshare.com>
2021-06-09 17:43:50 +0300

    Test 2

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -20,6 +20,7 @@ namespace Test
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+					
                     webBuilder.UseStartup<Startup>();
 					
                 });
